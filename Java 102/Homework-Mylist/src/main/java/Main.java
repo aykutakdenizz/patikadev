4efdf6c463ebd31b2ad5cf0e3f9daf0f9dbe19a6
aykutakdenizz <46684738+aykutakdenizz@users.noreply.github.com>
2022-03-12 13:23:20 +0300

    Homework - MyList (re-organize)

--- Java 102/Homework-Mylist/src/main/java/Main.java
+++ Java 102/Homework-Mylist/src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        MyList<Integer> liste = new MyList<>();
+        MyList<Integer> liste = new MyList<Integer>();
         System.out.println("Liste Durumu : " + (liste.isEmpty() ? "Boş" : "Dolu"));
         liste.add(10);
         liste.add(20);
